ae38e9f29f1d0bdb6e53d858d4f12991d8f01b1c
Jörg Battermann <jb@joergbattermann.com>
2016-04-10 18:43:56 +0200

    Sample code for '02 - Basics'

---
+++ 02-Basics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+
+namespace _02_Basics
+{
+    class Program
+    {
+        const string TeamProjectCollectionUri = @"http://win2012:8080/tfs/DefaultCollection"; // In my case this is a locally running development TFS (2015 Update 2) system
+        const int WorkItemIdentifier = 1; // This is the Work Item .Id which we will be retrieving
+
+        static void Main(string[] args)
+        {
+            var visualStudioServicesConnection = new VssConnection(new Uri(TeamProjectCollectionUri), new VssCredentials());
+            var workItemTrackingHttpClient = visualStudioServicesConnection.GetClient<WorkItemTrackingHttpClient>();
+            var workItemInstance = workItemTrackingHttpClient.GetWorkItemAsync(1).Result;
+
+            Console.WriteLine("Work Item {0} - '{1}' retrieved", workItemInstance.Id, workItemInstance.Fields["System.Title"]);
+        }
+    }
+}
