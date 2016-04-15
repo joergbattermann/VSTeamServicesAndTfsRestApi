e53b75ea4eccc3685ac75b09152a83e8ba5a0173
Jörg Battermann <jb@joergbattermann.com>
2016-04-15 13:36:29 +0200

    Preparing for Project (Collection) post

---
+++ 04-ProjectCollectionsAndProjects/Program.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Joerg Battermann">
+//   Copyright (c) 2016 Joerg Battermann. All rights reserved.
+// </copyright>
+// <author>Joerg Battermann</author>
+// -----------------------------------------------------------------------
+
+using System;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace _04_ProjectCollectionsAndProjects
+{
+    public class Program
+    {
+        const string BaseUri = @"http://win2012:8080/tfs/"; // In my case this is a locally running development TFS (2015 Update 2) system
+
+        static void Main(string[] args)
+        {
+            // instantiate a vss connection using the BaseUri (not the Project Collection Uri!)
+            var visualStudioServicesConnection = new VssConnection(new Uri(BaseUri), new VssCredentials());
+
+            // get ahold of the Project Collection client
+            var projectCollectionHttpClient = visualStudioServicesConnection.GetClient<ProjectCollectionHttpClient>();
+
+            // iterate over the first 10 Project Collections (I am allowed to see)
+            // however, if no parameter(s) were provided to the .GetProjectCollections() method, it would only retrieve one Collection,
+            // so basically this allows / provides fine-grained pagination control
+            foreach (var projectCollectionReference in projectCollectionHttpClient.GetProjectCollections(10, 0).Result)
+            {
+                // retrieve a reference to the actual project collection based on its (reference) .Id
+                var projectCollection = projectCollectionHttpClient.GetProjectCollection(projectCollectionReference.Id.ToString()).Result;
+
+                var webUrlForProjectCollection = projectCollection.Links.Links["web"] as ReferenceLink;
+
+                Console.WriteLine("Project Collection '{0}' (Id: {1}) at Url: '{2}' & API Url: '{3}'",
+                    projectCollection.Name,
+                    projectCollection.Id,
+                    webUrlForProjectCollection.Href,
+                    projectCollection.Url);
+            }
+        }
+    }
+}
---
+++ 04-ProjectCollectionsAndProjects/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Joerg Battermann">
+//   Copyright (c) 2016 Joerg Battermann. All rights reserved.
+// </copyright>
+// <author>Joerg Battermann</author>
+// -----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("04-ProjectCollectionsAndProjects")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("04-ProjectCollectionsAndProjects")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("0a4b018c-6984-4a1b-a3ac-5e701ed1e58f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
