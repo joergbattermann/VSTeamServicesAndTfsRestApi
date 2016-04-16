9d0b24adf790014627dead7aab91e16f2d6e3761
Jörg Battermann <jb@joergbattermann.com>
2016-04-16 21:53:29 +0200

    Code for Post 04 (Project Collections and Projects) finished

--- 04-ProjectCollectionsAndProjects/Program.cs
+++ 04-ProjectCollectionsAndProjects/Program.cs
@@ -6,9 +6,13 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.VisualStudio.Services.Client;
 using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.Operations;
 using Microsoft.VisualStudio.Services.WebApi;
 
 namespace _04_ProjectCollectionsAndProjects
@@ -87,6 +91,121 @@ namespace _04_ProjectCollectionsAndProjects
                     knownTeamProject.Capabilities["processTemplate"]["templateName"],
                     knownTeamProject.Capabilities["processTemplate"]["templateTypeId"]);
             }
+
+            // We can also create new projects, i.e. like this:
+            var newTeamProjectToCreate = new TeamProject();
+            var somewhatRandomValueForProjectName = (int)(DateTime.UtcNow - DateTime.UtcNow.Date).TotalSeconds;
+
+            // mandatory information is name,
+            newTeamProjectToCreate.Name = $"Dummy Project {somewhatRandomValueForProjectName}";
+            
+            // .. description
+            newTeamProjectToCreate.Description = $"This is a dummy project";
+
+            // and capabilities need to be provided
+            newTeamProjectToCreate.Capabilities = new Dictionary<string, Dictionary<string, string>>
+            {
+                {
+                    // particularly which version control the project shall use (as of writing 'TFVC' and 'Git' are available
+                    "versioncontrol", new Dictionary<string, string>()
+                    {
+                        {"sourceControlType", "TFVC"}
+                    }
+                },
+                {
+                    // and which Process Template to use
+                    "processTemplate", new Dictionary<string, string>()
+                    {
+                        {"templateTypeId", "adcc42ab-9882-485e-a3ed-7678f01f66bc"} // This is the Id for the Agile template, on my TFS server at least.
+                    }
+                }
+            };
+
+            // because project creation takes some time on the server, the creation is queued and you'll get back a 
+            // ticket / reference to the operation which you can use to track the progress and/or completion
+            var projectCreationOperationReference = projectHttpClientForKnownProjectCollection.QueueCreateProject(newTeamProjectToCreate).Result;
+            
+            Console.WriteLine("Project '{0}' Creation is '{1}'", newTeamProjectToCreate.Name, projectCreationOperationReference.Status);
+
+            // tracking the status via a OperationsHttpClient (for the Project collection)
+            var operationsHttpClientForKnownProjectCollection = projectVssConnectionForKnownProjectCollection.GetClient<OperationsHttpClient>();
+
+            var projectCreationOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectCreationOperationReference.Id).Result;
+            while (projectCreationOperation.Status != OperationStatus.Succeeded
+                && projectCreationOperation.Status != OperationStatus.Failed
+                && projectCreationOperation.Status != OperationStatus.Cancelled)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000); // yuck
+
+                projectCreationOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectCreationOperationReference.Id).Result;
+            }
+
+            // alright - creation is finished, successfully or not
+            Console.WriteLine("Project '{0}' Creation finished with State '{1}' & Message: '{2}'",
+                newTeamProjectToCreate.Name,
+                projectCreationOperation.Status,
+                projectCreationOperation.ResultMessage ?? "n.a.");
+
+            // we can also update a teamProject
+            // i.e. the just created one - which we need to retrieve first to get its .Id / Guid
+            var allTeamProjectsOfProjectCollection = projectHttpClientForKnownProjectCollection
+                .GetProjects(top: Int32.MaxValue, skip: 0).Result // don't do this in real code
+                .ToList();
+
+            var newTeamProjectReferenceForProjectJustCreated = allTeamProjectsOfProjectCollection
+                .FirstOrDefault(projectReference => string.Equals(projectReference.Name, newTeamProjectToCreate.Name));
+            var newTeamProjectJustCreated = projectHttpClientForKnownProjectCollection.GetProject(newTeamProjectReferenceForProjectJustCreated.Id.ToString()).Result;
+
+            newTeamProjectJustCreated.Description = "Some updated Description";
+
+            var projectUpdateOperationReference = projectHttpClientForKnownProjectCollection.UpdateProject(newTeamProjectJustCreated.Id, newTeamProjectJustCreated).Result; // .Id stays the same, even after updates
+
+            Console.WriteLine("Project '{0}' Update is '{1}'", newTeamProjectToCreate.Name, projectUpdateOperationReference.Status);
+
+            // and again, we track the queued deletion work / operation like before
+            var projectUpdateOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectUpdateOperationReference.Id).Result;
+            while (projectUpdateOperation.Status != OperationStatus.Succeeded
+                && projectUpdateOperation.Status != OperationStatus.Failed
+                && projectUpdateOperation.Status != OperationStatus.Cancelled)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000); // again, yuck
+
+                projectUpdateOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectUpdateOperationReference.Id).Result;
+            }
+
+            Console.WriteLine("Project '{0}' Update finished with State '{1}' & Message: '{2}'",
+                newTeamProjectJustCreated.Name,
+                projectUpdateOperation.Status,
+                projectUpdateOperation.ResultMessage ?? "n.a.");
+
+            // aaand we can also delete projects
+
+            // i.e. the new created / updated one
+            var projectDeletionOperationReference = projectHttpClientForKnownProjectCollection.QueueDeleteProject(newTeamProjectJustCreated.Id).Result; // .Id stays the same, even after updates
+
+            Console.WriteLine("Project '{0}' Deletion is '{1}'", newTeamProjectJustCreated.Name, projectCreationOperationReference.Status);
+
+            // and again, we track the queued deletion work / operation like before
+            var projectDeletionOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectDeletionOperationReference.Id).Result;
+            while (projectDeletionOperation.Status != OperationStatus.Succeeded
+                && projectDeletionOperation.Status != OperationStatus.Failed
+                && projectDeletionOperation.Status != OperationStatus.Cancelled)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000); // again, yuck
+
+                projectDeletionOperation = operationsHttpClientForKnownProjectCollection.GetOperation(projectDeletionOperationReference.Id).Result;
+            }
+
+            // alright - creation is finished, successfully or not
+            Console.WriteLine("Project '{0}' Deletion finished with State '{1}' & Message: '{2}'",
+                newTeamProjectJustCreated.Name,
+                projectCreationOperation.Status,
+                projectCreationOperation.ResultMessage ?? "n.a.");
+
+            
         }
     }
 }
