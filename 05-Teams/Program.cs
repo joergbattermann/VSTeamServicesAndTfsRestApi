be7cdf672ffc4b273a70bb5a0d521e2389c747ea
Jörg Battermann <jb@joergbattermann.com>
2016-04-18 20:46:54 +0200

    Preparing 05-Teams

--- 05-Teams/Program.cs
+++ 05-Teams/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JB.Common.TeamFoundationServer.Client.ExtensionMethods;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
 
 namespace _05_Teams
 {
-    class Program
+    public static class Program
     {
+        const string BaseUri = @"http://win2012:8080/tfs/"; // In my case this is a locally running development TFS (2015 Update 2) system
+
         static void Main(string[] args)
         {
+            // instantiate a vss connection using the BaseUri (not the Project Collection Uri!)
+            var visualStudioServicesConnection = new VssConnection(new Uri(BaseUri), new VssCredentials());
         }
     }
 }
