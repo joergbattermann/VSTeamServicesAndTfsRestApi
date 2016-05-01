fd834b6ab05e812169dea0202c08ed60b0f71a8b
Jörg Battermann <jb@joergbattermann.com>
2016-05-01 19:34:20 +0200

    more work on code for 06-Workitems retrieval / reading

--- 06-WorkItems-Reading/Program.cs
+++ 06-WorkItems-Reading/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Client;
@@ -9,9 +11,12 @@ namespace _06_WorkItems_Reading
     class Program
     {
         // the Base Uri used used the Project Collection one, in my case on a local development Server
-        const string TeamProjectCollectionUri = @"http://win2012:8080/tfs/DefaultCollection/";
+        static readonly string TeamProjectCollectionUri = @"http://win2012:8080/tfs/DefaultCollection/";
+        static readonly string ExistingProjectId = "dc68c474-2ce0-4be6-9617-abe97f66ec1e";
 
-        const int ExistingWorkItemId = 1; // This is the Work Item .Id which we will be retrieving
+        static readonly int ExistingWorkItemId = 1; // This is a single Work Item .Id known to exist (on my system)
+        static readonly List<int> ExistingWorkItemIds = new List<int>() { 1, 2, 3 ,4 };  // This is a list of Work Item .Id values known to exist (on my system)
+        static readonly Guid ExistingStoredQueryId = new Guid("31902604-31bf-4b59-9df1-eea6e8b61093");
 
         static void Main(string[] args)
         {
@@ -21,13 +26,44 @@ namespace _06_WorkItems_Reading
             // Get a Work Item Tracking client
             var workItemTrackingHttpClient = visualStudioServicesConnection.GetClient<WorkItemTrackingHttpClient>();
 
-            // This will include Work Item Relations (if any Relations exist at all - otherwise & unfortunately .Relations will be 'null' (rather than an empty List<>)
+
+            // 1. Retrieve a single Work Item by its .Id
+            // #############################
+
+            // This will include Work Item Relations (work item links, test result associations etc) via the 'expand' parameter
+            // However, if any no Relations exist at all .Relations will be 'null' (rather than an empty List<>)
             var workItemIncludingRelations = workItemTrackingHttpClient.GetWorkItemAsync(ExistingWorkItemId, expand: WorkItemExpand.Relations).Result;
 
-            // but this won't
+            // same as not specifying an 'expand' parameter at all - .Relations is 'null'
             var workItemExcludingRelations = workItemTrackingHttpClient.GetWorkItemAsync(ExistingWorkItemId).Result;
 
 
+            // 2. Retrieve multiple Work Items by their .Ids
+            // #############################
+
+            // You can also retrieve multiple work items in bulk via their .Id values in one request to the VSTS/TFS backend like this:
+            var multipleWorkItemsRetrievedByTheirId = workItemTrackingHttpClient.GetWorkItemsAsync(ExistingWorkItemIds, expand: WorkItemExpand.All).Result;
+
+            // 3. Query for Work Items
+
+            // 3.1 .. using a Wiql (Work Item Query Language) Query
+            var wiqlQuery = new Wiql() {Query = "Select * from WorkItems"};
+            var workItemQueryResultForWiqlBasedQuery = workItemTrackingHttpClient.QueryByWiqlAsync(wiqlQuery).Result;
+
+            var workItemsForQueryResultForWiqlBasedQuery = workItemTrackingHttpClient
+                .GetWorkItemsAsync(
+                    workItemQueryResultForWiqlBasedQuery.WorkItems.Select(workItemReference => workItemReference.Id),
+                    expand: WorkItemExpand.All).Result;
+
+            // 3.2 .. or by using a stored Query by its id
+            var workItemQueryResultForStoredQuery = workItemTrackingHttpClient.QueryByIdAsync(ExistingStoredQueryId).Result;
+
+            var workItemsForQueryResultForStoredQuery = workItemTrackingHttpClient
+                .GetWorkItemsAsync(
+                    workItemQueryResultForStoredQuery.WorkItems.Select(workItemReference => workItemReference.Id),
+                    expand: WorkItemExpand.All).Result;
+
+            // 4. Stored Queries
 
             var relationTypes = workItemTrackingHttpClient.GetRelationTypesAsync().Result;
         }
