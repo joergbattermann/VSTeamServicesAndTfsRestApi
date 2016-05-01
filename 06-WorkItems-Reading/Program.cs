657ab4f891a68e4f75d3fee4805d04e018f010fe
Jörg Battermann <jb@joergbattermann.com>
2016-05-01 19:54:54 +0200

    ... more of stored queries handling

--- 06-WorkItems-Reading/Program.cs
+++ 06-WorkItems-Reading/Program.cs
@@ -65,6 +65,53 @@ namespace _06_WorkItems_Reading
 
             // 4. Stored Queries
 
+            // Retrieve stored Queries (which you / your authenticated user can see and access), up to 2 (sub-)levels or -hierarchies deep.
+            // .. it appears that (currently?) you can specify a max value of '2' for the 'depth' parameter which means you might need to 
+            // retrieve queries deeper in the hierarchy using another approach:
+            // > check the corresponding QueryHierarchyItem for its .HasChildren having a Value (.HasValue==true) and that Value being 'true' BUT
+            // .. the .Children being 'null'. Go ahead and use that QueryHierarchyItem's .Path value for the .GetQueryAsync(projectId, queryHierarchyItemPath, ...) method
+            // .. to drill down further into the hierarchy
+            var allStoredQueriesAccessibleByAuthenticatedUser = workItemTrackingHttpClient.GetQueriesAsync(ExistingProjectId, expand: QueryExpand.All, depth: 2, includeDeleted: false).Result;
+
+            // then go ahead and use these queries (this should in real code be placed in a proper method / set of method(s) obviously
+            foreach (var storedQueryItem in allStoredQueriesAccessibleByAuthenticatedUser)
+            {
+                if (storedQueryItem.IsFolder.HasValue && storedQueryItem.IsFolder.Value == true)
+                {
+                    // this storedQueryItem is a Folder, it may have children.. or not
+                    if (storedQueryItem.HasChildren.HasValue && storedQueryItem.HasChildren.Value == true)
+                    {
+                        if (storedQueryItem.Children != null)
+                        {
+                            foreach (var childQueryItem in storedQueryItem.Children)
+                            {
+                                // iterate over child items.. and so and and so on and so on
+                            }
+                        }
+                        else
+                        {
+                            // this folder HAS children, but the deeper hierarchy hasn't been retrieved, yet
+                            // > see note above how to do just that
+                        }
+                    }
+                    else
+                    {
+                        // this query folder is empty
+                    }
+                }
+                else if (storedQueryItem.IsFolder.HasValue && storedQueryItem.IsFolder.Value == false)
+                {
+                    // this storedQueryItem is a query (and not a folder)
+                    // you can use it to run the query.. or access / modify etc its query statement
+                    var resultsForStoredQuery = workItemTrackingHttpClient.QueryByIdAsync(storedQueryItem.Id).Result;
+                }
+                else
+                {
+                    // this 'should' not happen
+                    throw new InvalidOperationException($"Well this is odd - QueryHierarchyItem '{storedQueryItem.Id}' is neither a folder, nor a query");
+                }
+            }
+
             var relationTypes = workItemTrackingHttpClient.GetRelationTypesAsync().Result;
         }
     }
